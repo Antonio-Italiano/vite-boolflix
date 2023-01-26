93da57d6ff3e389c9af26e55adec65073998855d
Antonio-Italiano <antoitalia221@gmail.com>
2023-01-26 19:26:10 +0100

    created & completed BaseSearch

---
+++ src/components/BaseSearch.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'BaseSearch',
+    data() {
+        return {
+
+            term: '',
+        }
+    },
+    props: {
+        placeholder: String
+    },
+    emits: ['change-term', 'form-submit']
+}
+</script>
+
+<template>
+    <form class="" @submit.prevent="$emit('form-submit')">
+        <input v-model.trim="term" type="text" :placeholder="placeholder || 'cerca'"
+            @keyup="$emit('change-term', term)">
+        <button class="btn btn-primary">invia</button>
+    </form>
+</template>
+
+<style>
+
+</style>
