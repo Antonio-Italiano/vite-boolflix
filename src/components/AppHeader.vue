f1dd5bac02452e48c3293ade91bda6f265e3da9a
Antonio-Italiano <antoitalia221@gmail.com>
2023-01-26 19:29:22 +0100

    link between AppHeader and BaseSearch using $emit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script>
+import BaseSearch from './BaseSearch.vue';
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+        }
+    },
+    components: { BaseSearch },
+    emits: ['research', 'send-research']
+}
+</script>
+
+<template>
+    <header>
+        <base-search @change-term="$emit('research', $event)" @form-submit="$emit('send-research')"
+            placeholder="cerca un film..."></base-search>
+    </header>
+</template>
+
+<style>
+
+</style>
