f4867232556ae36a7dda4ddad7a18f5c0e226569
Antonio-Italiano <antoitalia221@gmail.com>
2023-01-26 19:30:18 +0100

    created index.js

---
+++ src/data/index.js
@@ -0,0 +1,12 @@
+const apiUriMovie = '/search/movie';
+const apiUriTv = '/search/tv';
+
+const config = {
+    params: {
+        baseUri: 'https://api.themoviedb.org/3',
+        api_key: '?api_key=60c994a9cf0f3146570d9eecac46737d',
+        language: 'IT-it',
+        query: '',
+    }
+}
+export { apiUriMovie, apiUriTv, config };
