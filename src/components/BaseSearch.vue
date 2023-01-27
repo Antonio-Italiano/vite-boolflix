3f22f4ada431ed8d58bcf73bb3f584b0a9c53a97
Antonio-Italiano <antoitalia221@gmail.com>
2023-01-27 17:07:54 +0100

    Added icon BaseSearch

--- src/components/BaseSearch.vue
+++ src/components/BaseSearch.vue
@@ -3,25 +3,36 @@ export default {
     name: 'BaseSearch',
     data() {
         return {
-
             term: '',
+            asClicked: false,
         }
     },
     props: {
         placeholder: String
     },
-    emits: ['change-term', 'form-submit']
+    emits: ['change-term', 'form-submit'],
+    methods: {
+        changeParam() {
+            this.asClicked = !this.asClicked;
+            this.term = '';
+        }
+    }
 }
 </script>
 
 <template>
-    <form class="" @submit.prevent="$emit('form-submit')">
-        <input v-model.trim="term" type="text" :placeholder="placeholder || 'cerca'"
-            @keyup="$emit('change-term', term)">
-        <button class="btn btn-primary">invia</button>
+    <div v-if="!asClicked" class="search" @click="changeParam">
+        <font-awesome-icon class="text-white cursore" icon="fa-solid fa-magnifying-glass" />
+    </div>
+
+    <form v-if="asClicked" class="" @submit.prevent="$emit('form-submit')">
+        <input v-model.trim="term" type="text" :placeholder="placeholder || 'cerca'" @keyup="$emit('change-term', term)"
+            @keyup.enter="changeParam">
     </form>
 </template>
 
-<style>
-
+<style lang="scss" scoped>
+.search {
+    cursor: pointer;
+}
 </style>
